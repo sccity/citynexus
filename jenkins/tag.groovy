b3ead165f5b479c0628c6b97ef36f05d9b859c15
Ryan Levsey <rlevsey@santaclarautah.gov>
2025-03-07 14:23:59 -0500

    fix: use GitHub App authentication for tag pushing

--- jenkins/tag.groovy
+++ jenkins/tag.groovy
@@ -17,16 +17,12 @@ if (!supportedBranches.contains(branch)) {
 sh 'git config --global user.email "jenkins@santaclarautah.gov"'
 sh 'git config --global user.name "Jenkins CI"'
 
-withCredentials([usernamePassword(credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
-    sh '''
-        git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/sccity/citynexus.git
-    '''
+withCredentials([gitHubApp(appCredentialsId: 'jenkins-app-auth')]) {
+    sh "git tag -a v${commitHash} -m 'Build ${commitHash}'"
+    sh "git tag -a v${commitHash}-${branch} -m 'Build ${commitHash} on ${branch}'"
+    sh "git push origin v${commitHash}"
+    sh "git push origin v${commitHash}-${branch}"
 }
-
-sh "git tag -a v${commitHash} -m 'Build ${commitHash}'"
-sh "git tag -a v${commitHash}-${branch} -m 'Build ${commitHash} on ${branch}'"
-sh "git push origin v${commitHash}"
-sh "git push origin v${commitHash}-${branch}"
 
 writeFile file: 'branch.txt', text: branch
 writeFile file: 'commit_hash.txt', text: commitHash
