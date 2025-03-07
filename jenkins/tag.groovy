952d082adac16409845432330a341d59f26993a4
Ryan Levsey <rlevsey@santaclarautah.gov>
2025-03-07 14:02:25 -0500

    fix: add GitHub credentials for tag pushing

--- jenkins/tag.groovy
+++ jenkins/tag.groovy
@@ -1,10 +1,6 @@
 def branch = env.BRANCH_NAME
 if (!branch) {
-    // Try to get branch from remote tracking branch
-    branch = sh(script: 'git branch -r --contains HEAD | grep -v HEAD | sed "s/origin\\///" | head -n1 | tr -d " "', returnStdout: true).trim()
-    if (!branch) {
-        error("Could not determine branch name")
-    }
+    branch = sh(script: 'git rev-parse --abbrev-ref HEAD', returnStdout: true).trim()
 }
 def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 
@@ -15,9 +11,13 @@ if (!supportedBranches.contains(branch)) {
     error("Unsupported branch '${branch}'")
 }
 
-// Configure Git user
-sh 'git config --global user.email "jenkins@santaclarautah.gov"'
-sh 'git config --global user.name "Jenkins CI"'
+withCredentials([usernamePassword(credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
+    sh '''
+        git config --global user.email "jenkins@santaclarautah.gov"
+        git config --global user.name "Jenkins CI"
+        git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/sccity/citynexus.git
+    '''
+}
 
 sh "git tag -a v${commitHash} -m 'Build ${commitHash}'"
 sh "git tag -a v${commitHash}-${branch} -m 'Build ${commitHash} on ${branch}'"
