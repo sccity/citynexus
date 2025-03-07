f9e359bcb5bc72c2f4e01a39103be7886789ab1e
Ryan Levsey <rlevsey@santaclarautah.gov>
2025-03-07 14:31:24 -0500

    fix: correct variable interpolation and URL format

--- jenkins/tag.groovy
+++ jenkins/tag.groovy
@@ -18,13 +18,11 @@ sh 'git config --global user.email "jenkins@santaclarautah.gov"'
 sh 'git config --global user.name "Jenkins CI"'
 
 withCredentials([usernamePassword(credentialsId: 'git', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
-    sh '''
-        git remote set-url origin "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/sccity/citynexus.git"
-        git tag -a v${commitHash} -m "Build ${commitHash}"
-        git tag -a v${commitHash}-${branch} -m "Build ${commitHash} on ${branch}"
-        git push origin v${commitHash}
-        git push origin v${commitHash}-${branch}
-    '''
+    sh "git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/sccity/citynexus.git"
+    sh "git tag -a v${commitHash} -m 'Build ${commitHash}'"
+    sh "git tag -a v${commitHash}-${branch} -m 'Build ${commitHash} on ${branch}'"
+    sh "git push origin v${commitHash}"
+    sh "git push origin v${commitHash}-${branch}"
 }
 
 writeFile file: 'branch.txt', text: branch
